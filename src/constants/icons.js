3dae592abe84b113b0f3898ea4b4cc14d102d9c2
fedeemilo <fedmilo91@gmail.com>
2025-05-30 20:08:42 -0300

    Change bg color

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,9 +1,5 @@
-import FLAG_ES from '../assets/flag-es.svg'
-import FLAG_EN from '../assets/flag-en.svg'
 import ME from '../assets/me.png'
 import ME_WEBP from '../assets/me.webp'
-import ME_ABOUT from '../assets/me-about.png'
-import ME_ABOUT_WEBP from '../assets/me-about.webp'
 import PORTFOLIO_TRAVEL_ADVISOR from '../assets/portfolio-traveladvisor.png'
 import PORTFOLIO_TRAVEL_ADVISOR_WEBP from '../assets/portfolio-traveladvisor.webp'
 import PORTFOLIO_CRYPTOLAND from '../assets/portfolio-cryptoland.png'
@@ -16,16 +12,12 @@ import PORTFOLIO_TSHIRTS from '../assets/portfolio-tshirts.png'
 import PORTFOLIO_TSHIRTS_WEBP from '../assets/portfolio-tshirts.webp'
 import PORTFOLIO_MAMOVIES from '../assets/portfolio-mamovies.png'
 import PORTFOLIO_MAMOVIES_WEBP from '../assets/portfolio-mamovies.webp'
-import PORTFOLIO_CODEFIX from '../assets/portfolio8.png'
-import PORTFOLIO_CODEFIX_WEBP from '../assets/portfolio8.webp'
+import PORTFOLIO_CODEFIX from '../assets/portfolio-codefix.png'
+import PORTFOLIO_CODEFIX_WEBP from '../assets/portfolio-codefix.webp'
 
 export const ICONS = {
-    FLAG_ES,
-    FLAG_EN,
     ME,
     ME_WEBP,
-    ME_ABOUT,
-    ME_ABOUT_WEBP,
     PORTFOLIO_TRAVEL_ADVISOR,
     PORTFOLIO_TRAVEL_ADVISOR_WEBP,
     PORTFOLIO_CRYPTOLAND,
